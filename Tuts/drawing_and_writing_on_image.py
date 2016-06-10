f7e8cd24d85a8af93b041f37b88d1552e5eb56f1
Dave Ho <daveho8888@gmail.com>
2016-06-10 14:52:20 -0700

    update lesson 3

--- Tuts/drawing_and_writing_on_image.py
+++ Tuts/drawing_and_writing_on_image.py
@@ -10,8 +10,12 @@ import cv2
 
 img = cv2.imread("watch.jpg", cv2.IMREAD_COLOR)
 
-cv2.line(img,(0,0), (150, 150), (255, 255, 255), 15)
-
+#cv2.line(img,(0,0), (150, 150), (255, 255, 255), 15)
+#draw a line
+#cv2.rectangle(img, (15, 25), (200, 150), (0, 0, 255), 15)
+#draw a rectangle
+cv2.circle(img, (100, 63), 55, (0, 255, 0), -1)
+#draw a circle
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
