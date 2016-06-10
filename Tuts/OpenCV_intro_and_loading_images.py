4ab191ce77e4dcf1791f44851a4fe52a62caaa3e
Dave Ho <daveho8888@gmail.com>
2016-06-09 22:01:25 -0700

    update lesson 1

--- Tuts/OpenCV_intro_and_loading_images.py
+++ Tuts/OpenCV_intro_and_loading_images.py
@@ -14,3 +14,11 @@ img = cv2.imread('watch.jpg', cv2.IMREAD_GRAYSCALE) # Read in Image as grayscale
 cv2.imshow('image', img) # show image to the screen
 cv2.waitKey(0) # wait for key press on keyboard
 cv2.destroyAllWindows() # destroy all image displays
+
+#plotting with matplotlib
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.plot([200,300,400],[100,200,300],'c', linewidth=5)
+plt.show()
+
+# cv2.imwrite('watchgray.png',img) # writing to a file to the local disk
