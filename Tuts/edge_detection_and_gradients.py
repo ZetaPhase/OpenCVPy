dc9689cbb7a6390a4b5cb1f97be94e1c4117c337
Dave Ho <daveho8888@gmail.com>
2016-06-14 08:30:03 -0700

    updates to lesson 9

--- Tuts/edge_detection_and_gradients.py
+++ Tuts/edge_detection_and_gradients.py
@@ -15,14 +15,22 @@ while(1):
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    lower_red = np.array([30,150,50])
-    upper_red = np.array([255,255,180])
+    #lower_red = np.array([30,150,50])
+    #upper_red = np.array([255,255,180])
     
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+    #mask = cv2.inRange(hsv, lower_red, upper_red)
+    #res = cv2.bitwise_and(frame,frame, mask= mask)
+
+
+    laplacian = cv2.Laplacian(frame,cv2.CV_64F)
+    sobelx = cv2.Sobel(frame,cv2.CV_64F,1,0,ksize=5)
+    sobely = cv2.Sobel(frame,cv2.CV_64F,0,1,ksize=5)
+    edges = cv2.Canny(frame,100,200)
 
     cv2.imshow('Original',frame)
-    edges = cv2.Canny(frame,100,200)
+    cv2.imshow('laplacian', laplacian)
+    cv2.imshow('sobelx', sobelx)
+    cv2.imshow('sobely', sobely)
     cv2.imshow('Edges',edges)
 
     k = cv2.waitKey(5) & 0xFF
