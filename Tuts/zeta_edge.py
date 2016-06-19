d5886e2110229831c1db1d854ca2b36c6fa93686
Dave Ho <daveho8888@gmail.com>
2016-06-18 22:33:00 -0700

    changes to zetaedge

--- Tuts/zeta_edge.py
+++ Tuts/zeta_edge.py
@@ -8,12 +8,34 @@ Created on Fri Jun 17 09:14:05 2016
 import cv2
 import numpy as np
 
+img = cv2.imread('mypic.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+print img.shape
+print gray.shape
+
+'''
 first_time = True
 
 cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
+    
+    # convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)    
+    
+    
+    #vertical check
+    
+    #horizontal check
+    
+    #threshold
+    
+    #resize image
+    
+    #display image
+    
+    #testing
     if first_time:
         print frame
         print type(frame)
@@ -25,3 +47,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+'''
